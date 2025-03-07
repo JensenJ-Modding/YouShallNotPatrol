3aa42d1e19a1fdfa15f04e4e99f5a15288fb64d5
Jensen <jensenbradley@icloud.com>
2025-03-07 04:57:46 +0000

    Fix config duplicate keys

--- common/src/main/java/net/youshallnotpatrol/config/ServerConfig.java
+++ common/src/main/java/net/youshallnotpatrol/config/ServerConfig.java
@@ -22,14 +22,14 @@ public class ServerConfig {
         pillagerSpawnOnDifferentPlayer = BUILDER.comment("If possible, should pillager patrols be unable to spawn on the same player twice in a row.")
                 .define("pillagerSpawnOnDifferentPlayer", true);
         pillagerSpawnChance = BUILDER.comment("The spawn chance (%) for a pillager patrol to spawn on a selected player when an attempt is made.")
-                .defineInRange("pillagerSpawnOnDifferentPlayer", 20, 0, 100);
+                .defineInRange("pillagerSpawnChance", 20, 0, 100);
         BUILDER.pop();
 
         BUILDER.comment("Wandering Trader Settings").push(CATEGORY_TRADER);
         traderSpawnOnDifferentPlayer = BUILDER.comment("If possible, should wandering traders be unable to spawn on the same player twice in a row.")
                 .define("pillagerSpawnOnDifferentPlayer", true);
         traderSpawnChance = BUILDER.comment("The spawn chance (%) for a wandering trader to spawn on a selected player when an attempt is made.")
-                .defineInRange("pillagerSpawnOnDifferentPlayer", 10, 0, 100);
+                .defineInRange("traderSpawnChance", 10, 0, 100);
         BUILDER.pop();
         SERVER_CONFIG = BUILDER.build();
     }
